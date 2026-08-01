05769fc8b8da642f19877a4369e95e1f24d5b5e3
Emzymodios <nguyenquangphuoc21@gmail.com>
2026-08-02 06:05:11 +0700

    Create esp_tab.lua

---
+++ Scripts/ui/tabs/esp_tab.lua
@@ -0,0 +1,13 @@
+local ESPTab = {}
+
+function ESPTab.Create(otherContainer, UI)
+    Elements.CreateSlider(otherContainer, 15, "ESP Text Size", 8, 30, 12, function(val)
+        if UI.OnESPTextSizeChanged then UI.OnESPTextSizeChanged(val) end
+    end)
+
+    Elements.CreateToggleRow(otherContainer, 80, "ESP Player", function(state)
+        if UI.OnESPToggled then UI.OnESPToggled(state) end
+    end)
+end
+
+return ESPTab
