049ae4bde865ae86be7293ebbfe17d5281d076c0
Emzymodios <nguyenquangphuoc21@gmail.com>
2026-08-01 18:28:38 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -41,11 +41,16 @@ UI.OnJumpToggled = function(state)
     Logic.ToggleInfiniteJump(state)
 end
 
+-- Kết nối sự kiện FPS Boost
+UI.OnFPSBoostToggled = function(state)
+    Logic.ToggleFPSBoost(state)
+end
+
 UI.OnESPToggled = function(state)
     Logic.ToggleESP(state)
 end
 
--- Thêm đoạn kết nối này để nhận sự kiện đổi kích thước chữ ESP
+-- Kết nối sự kiện đổi kích thước chữ ESP
 UI.OnESPTextSizeChanged = function(value)
     Logic.SetESPTextSize(value)
 end
