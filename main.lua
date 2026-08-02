e6c997b5d6fd60747839a8edf046c748c679fe97
Emzymodios <nguyenquangphuoc21@gmail.com>
2026-08-02 07:34:20 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -43,6 +43,8 @@ UI.OnAimbotTargetChanged = function(targetName) Logic.SetAimbotTarget(targetName
 UI.OnTeleportPlayerToggled = function(state, targetName) 
     Logic.ToggleTeleportPlayer(state, targetName) 
 end
+-- Cầu nối Godmode
+UI.OnGodmodeToggled = function(state) Logic.ToggleGodmode(state) end
 
 -- Cầu nối Reset cấu hình tổng thể
 UI.OnResetClicked = function() Logic.ResetConfig() end
