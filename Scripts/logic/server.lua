f1d703f2a2231d99ee46cde47c34b19cdb9f3501
Emzymodios <nguyenquangphuoc21@gmail.com>
2026-08-02 22:17:08 +0700

    Update server.lua

--- Scripts/logic/server.lua
+++ Scripts/logic/server.lua
@@ -8,8 +8,6 @@ local ServerModule = {}
 function ServerModule.Rejoin()
     local success, err = pcall(function()
         if #Players:GetPlayers() <= 1 then
-            Players.LocalPlayer:Kick("\nRejoining...")
-            task.wait(1)
             TeleportService:Teleport(game.PlaceId, Players.LocalPlayer)
         else
             TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, Players.LocalPlayer)
@@ -23,23 +21,34 @@ end
 function ServerModule.ServerHop()
     local success, err = pcall(function()
         local servers = {}
-        local req = game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")
-        local body = HttpService:JSONDecode(req)
+        local cursor = ""
         
-        if body and body.data then
-            for _, server in ipairs(body.data) do
-                if server.playing < server.maxPlayers and server.id ~= game.JobId then
-                    table.insert(servers, server.id)
-                end
+        -- Lấy danh sách server công khai từ API Roblox
+        repeat
+            local url = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+            if cursor ~= "" then
+                url = url .. "&cursor=" .. cursor
             end
-        end
+            
+            local req = game:HttpGet(url)
+            local body = HttpService:JSONDecode(req)
+            
+            if body and body.data then
+                for _, server in ipairs(body.data) do
+                    if server.playing < server.maxPlayers and server.id ~= game.JobId then
+                        table.insert(servers, server.id)
+                    end
+                end
+                cursor = body.nextPageCursor
+            else
+                break
+            end
+        until #servers > 0 or not cursor
         
         if #servers > 0 then
             TeleportService:TeleportToPlaceInstance(game.PlaceId, servers[math.random(1, #servers)], Players.LocalPlayer)
         else
-            if UI and UI.Notify then
-                UI.Notify("Không tìm thấy server trống phù hợp!", 3)
-            end
+            warn("Không tìm thấy server trống phù hợp!")
         end
     end)
     if not success then
