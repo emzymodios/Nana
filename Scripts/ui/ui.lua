8a724af11f5ead5ae8b3bb7f3f30b8d09afa79c6
Emzymodios <nguyenquangphuoc21@gmail.com>
2026-08-02 22:11:05 +0700

    Update ui.lua

--- Scripts/ui/ui.lua
+++ Scripts/ui/ui.lua
@@ -103,7 +103,7 @@ function UI.Init()
     titleLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
     titleLabel.TextSize = 16
     titleLabel.Font = Enum.Font.GothamBlack
-    titleLabel.Text = "NANA HUB 1.4"
+    titleLabel.Text = "NANA HUB 1.5"
     titleLabel.TextXAlignment = Enum.TextXAlignment.Left
     titleLabel.Parent = topBar
 
