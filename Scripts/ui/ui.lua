4bab23b2843b321458331d2ba309e2d78133648d
Oni Studio <nguyenquangphuoc21@gmail.com>
2026-08-31 23:32:57 +0700

    Update ui.lua

--- Scripts/ui/ui.lua
+++ Scripts/ui/ui.lua
@@ -123,7 +123,7 @@ function UI.Init()
     titleLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
     titleLabel.TextSize = 16
     titleLabel.Font = Enum.Font.GothamBlack
-    titleLabel.Text = "NANA HUB 1.7"
+    titleLabel.Text = "NANA HUB 1.8"
     titleLabel.TextXAlignment = Enum.TextXAlignment.Left
     titleLabel.Parent = topBar
 
