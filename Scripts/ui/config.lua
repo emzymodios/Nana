933c37578d9ad2057182e11b3da207c5e3083d8c
Oni Studio <nguyenquangphuoc21@gmail.com>
2026-09-12 12:38:30 +0700

    Add BackgroundImageId to config.lua

--- Scripts/ui/config.lua
+++ Scripts/ui/config.lua
@@ -3,7 +3,7 @@ local Config = {}
 
 -- ID ảnh nút mở Hub
 Config.IconImageId = "rbxassetid://130473788814906"
-
+Config.BackgroundImageId = "rbxassetid://116222439691339"
 -- = 0.45
 
 return Config
